3d4b58812a44701595b2378f658979fa273d161a
Pratham2316 <prathamsinghps8@gmail.com>
2025-04-03 22:59:09 +0530

    Create SimpleForm.jsx

---
+++ WEEK-3/SimpleForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+// 2. Form Component
+function SimpleForm() {
+    const [inputValue, setInputValue] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputValue);
+    };
+    return (
+        <form onSubmit={handleSubmit} className="p-4">
+            <input 
+                type="text" 
+                className="border p-2" 
+                value={inputValue} 
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button type="submit" className="bg-blue-500 text-white p-2 ml-2">Submit</button>
+        </form>
+    );
+}
