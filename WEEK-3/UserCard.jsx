d508d0291994bb8c1979bf312a92f1df918228a5
Pratham2316 <prathamsinghps8@gmail.com>
2025-04-03 22:59:45 +0530

    Create UserCard.jsx

---
+++ WEEK-3/UserCard.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+// 3. UserCard Component
+function UserCard({ name, email }) {
+    return (
+        <div className="border p-4 rounded-lg shadow-md w-64">
+            <h2 className="text-lg font-bold">{name}</h2>
+            <p className="text-gray-600">{email}</p>
+        </div>
+    );
+}
